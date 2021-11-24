0e5fd4a9efbd966d1c8662f5440201c15634029f
YellowFlash2012 <cyclop9@hotmail.com>
2021-11-24 09:53:09 +0100

    services and contact sections of the homepage completed. Also added functionality to collect email of the optin form via formspree.io

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,37 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join our newsletter and get 20% off</h3>
+
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+            quia ea dolore ex vitae temporibus porro doloremque, ab esse eaque!
+          </p>
+
+          <form
+            action="https://formspree.io/f/xyyogzne"
+            className="contact-form"
+            method="POST"
+          >
+            <input
+              type="email"
+              className="form-input"
+              placeholder="enter email"
+              name="_replyto"
+            />
+
+            <button type="submit" className="submit-btn">
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
