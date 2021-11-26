18c90060c973b32872e43eebb8382d4afe58a4fd
YellowFlash2012 <cyclop9@hotmail.com>
2021-11-26 05:45:00 +0100

    colors and amount buttons functionalities of the addtocart component completed

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -82,7 +82,7 @@ const SingleProductPage = () => {
 
             <hr />
             
-            {stock > 0 && <AddToCart />}
+            {stock > 0 && <AddToCart product={product} />}
           </section>
         </div>
       </div>
