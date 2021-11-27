084e106fce4e3084d60083720ba4cbf07cbf36ce
YellowFlash2012 <cyclop9@hotmail.com>
2021-11-27 06:28:15 +0100

    price, shipping & clear filters completed

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -71,6 +71,22 @@ const filter_reducer = (state, action) => {
   if (action.type === FILTER_PRODUCTS) {
     return { ...state }
   }
+
+  if (action.type === CLEAR_FILTERS) {
+    return {
+      ...state,
+      filters: {
+        ...state.price,
+        text: "",
+        company: "all",
+        category: "all",
+        color: "all",
+        
+        price: state.filters.max_price,
+        shipping: false,
+      },
+    };
+  }
   
   throw new Error(`No Matching "${action.type}" - action type`)
 }
