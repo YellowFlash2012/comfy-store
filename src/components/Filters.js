6d763ce56697b81c5df5be086c6af3d912c482ad
YellowFlash2012 <cyclop9@hotmail.com>
2021-11-27 05:40:54 +0100

    filter by brand completed

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -57,6 +57,26 @@ const Filters = () => {
             </div>
           </div>
           {/* end of categories */}
+
+          {/* companies */}
+          <div className="form-control">
+            <h5>company</h5>
+            <select
+              name="company"
+              value={company}
+              onChange={updateFilters}
+              className="company"
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} values={c}>
+                    {c}
+                  </option>
+                )
+              })}
+            </select>
+          </div>
+          {/* end of companies */}
         </form>
       </div>
     </Wrapper>
