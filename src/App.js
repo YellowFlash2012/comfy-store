3391aed7cea917a27c5482ab7be1ecbac45b1ba4
YellowFlash2012 <cyclop9@hotmail.com>
2021-11-29 06:33:25 +0100

    authwrapper completed

--- src/App.js
+++ src/App.js
@@ -11,43 +11,50 @@ import {
   Error,
   Checkout,
   PrivateRoute,
+  AuthWrapper
 } from "./pages";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-        <Route exact path="/about">
-          <About />
-        </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
 
-        <Route exact path="/cart">
-          <Cart />
-        </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
 
-        <Route exact path="/products">
-          <Products />
-        </Route>
+          <Route exact path="/products">
+            <Products />
+          </Route>
 
-        <Route exact path="/products/:id" children={<SingleProduct />}></Route>
+          <Route
+            exact
+            path="/products/:id"
+            children={<SingleProduct />}
+          ></Route>
 
-        <PrivateRoute exact path="/checkout">
-          <Checkout />
-        </PrivateRoute>
+          <PrivateRoute exact path="/checkout">
+            <Checkout />
+          </PrivateRoute>
 
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
-      <Footer/>
-    </Router>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
